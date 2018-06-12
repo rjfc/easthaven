9cf79a9d71eca013b45700d922e36df1642ab3d6
rjc <russelljfchan@gmail.com>
2018-06-12 00:53:52 -0400

    Dragging only works when rules of placement are followed and the dealing deck can deal to the piles

--- DeckClass.java
+++ DeckClass.java
@@ -1,163 +1,118 @@
 import java.awt.*;
 import java.util.*;
 
-public class DeckClass extends ShapeClass
-{
-    Vector deck = new Vector ();
-    String size;
+public class DeckClass extends ShapeClass {
+	Vector deck = new Vector();
+	String size;
 
-    public void setSize (String newSize)
-    {
-	size = newSize;
-	if (size.equals ("sm"))
-	{
-	    setHeight (60);
+	public void setSize(String newSize) {
+		size = newSize;
+		if (size.equals("sm")) {
+			setHeight(60);
+		} else if (size.equals("md")) {
+			setHeight(80);
+		} else if (size.equals("lg")) {
+			setHeight(100);
+		} else if (size.equals("xl")) {
+			setHeight(120);
+		}
+		setWidth((int) (getHeight() * 0.7));
 	}
-	else if (size.equals ("md"))
-	{
-	    setHeight (80);
+
+	public void addCard(CardClass newCard) {
+		deck.addElement(newCard);
 	}
-	else if (size.equals ("lg"))
-	{
-	    setHeight (100);
+
+	public void addCard(CardClass newCard, int pos) {
+		Object tempCard1, tempCard2;
+		deck.setSize(deck.size() + 1);
+		tempCard1 = deck.get(deck.size() - pos);
+		deck.setElementAt(newCard, deck.size() - pos);
+		for (int i = deck.size() - pos + 1; i < deck.size(); i++) {
+			tempCard2 = deck.get(i);
+			deck.setElementAt(tempCard1, i);
+			tempCard1 = tempCard2;
+		}
 	}
-	else if (size.equals ("xl"))
-	{
-	    setHeight (120);
+
+	public boolean removeTopCard() {
+		if (deck.size() > 0) {
+			deck.removeElement(deck.lastElement());
+			return true;
+		} else {
+			return false;
+		}
 	}
-	setWidth ((int) (getHeight () * 0.7));
-    }
 
-
-    public void addCard (CardClass newCard)
-    {
-	deck.addElement (newCard);
-    }
-
-
-    public void addCard (CardClass newCard, int pos)
-    {
-	Object tempCard1, tempCard2;
-	deck.setSize (deck.size () + 1);
-	tempCard1 = deck.get (deck.size () - pos);
-	deck.setElementAt (newCard, deck.size () - pos);
-	for (int i = deck.size () - pos + 1 ; i < deck.size () ; i++)
-	{
-	    tempCard2 = deck.get (i);
-	    deck.setElementAt (tempCard1, i);
-	    tempCard1 = tempCard2;
+	public boolean removeBottomCard() {
+		if (deck.size() > 0) {
+			deck.removeElement(deck.firstElement());
+			return true;
+		} else {
+			return false;
+		}
 	}
-    }
 
-
-    public boolean removeTopCard ()
-    {
-	if (deck.size () > 0)
-	{
-	    deck.removeElement (deck.lastElement ());
-	    return true;
+	public boolean removeCard(CardClass rmCard) {
+		if (deck.size() > 0) {
+			deck.removeElement(rmCard);
+			return true;
+		} else {
+			return false;
+		}
 	}
-	else
-	{
-	    return false;
+
+	public Object getTopCard() {
+		return deck.lastElement();
 	}
-    }
 
-
-    public boolean removeBottomCard ()
-    {
-	if (deck.size () > 0)
-	{
-	    deck.removeElement (deck.firstElement ());
-	    return true;
+	public Object getBottomCard() {
+		return deck.firstElement();
 	}
-	else
-	{
-	    return false;
+
+	public Object getCardAt(int index) {
+		return deck.get(index);
 	}
-    }
 
-
-    public boolean removeCard (CardClass rmCard)
-    {
-	if (deck.size () > 0)
-	{
-	    deck.removeElement (rmCard);
-	    return true;
+	public int getDeckLength() {
+		return deck.size();
 	}
-	else
-	{
-	    return false;
+
+	public boolean shuffle() {
+		if (deck.size() > 0) {
+			Collections.shuffle(deck);
+			return true;
+		} else {
+			return false;
+		}
 	}
-    }
 
-
-    public Object getTopCard ()
-    {
-	return deck.lastElement ();
-    }
-
-
-    public Object getBottomCard ()
-    {
-	return deck.lastElement ();
-    }
-
-
-    public int getDeckLength ()
-    {
-	return deck.size ();
-    }
-
-
-    public boolean shuffle ()
-    {
-	if (deck.size () > 0)
-	{
-	    Collections.shuffle (deck);
-	    return true;
+	public void standardDeck() {
+		CardClass c1;
+		for (int i = 0; i < 4; i++) {
+			for (int j = 1; j < 14; j++) {
+				c1 = new CardClass();
+				c1.setSuit(i);
+				c1.setValue(j);
+				addCard(c1);
+			}
+		}
 	}
-	else
-	{
-	    return false;
+
+	public void draw(Graphics g) {
+		for (int i = 0; i < deck.size(); i++) {
+			CardClass c1 = (CardClass) deck.get(i);
+			c1.setSize(size);
+			c1.setCentre(getCentreX() - i / 4, getCentreY() - i / 4);
+			c1.setSide(0);
+			c1.draw(g);
+		}
 	}
-    }
 
-
-    public void standardDeck ()
-    {
-	CardClass c1;
-	for (int i = 0 ; i < 4 ; i++)
-	{
-	    for (int j = 1 ; j < 14 ; j++)
-	    {
-		c1 = new CardClass ();
-		c1.setSuit (i);
-		c1.setValue (j);
-		addCard (c1);
-	    }
+	public void erase(Graphics g) {
+		Color cOldColor = iColour;
+		g.setColor(Color.white);
+		g.fillRect(iCentreX - iWidth / 2, iCentreY - iHeight / 2, iWidth + 1, iHeight + 1);
+		setColor(cOldColor);
 	}
-    }
-
-
-    public void draw (Graphics g)
-    {
-	if (deck.size () > 0)
-	{
-	    CardClass c1 = (CardClass) deck.lastElement ();
-	    c1.setSize (size);
-	    c1.setCentre (getCentreX (), getCentreY ());
-	    c1.setSide (0);
-	    c1.draw (g);
-	}
-    }
-
-
-    public void erase (Graphics g)
-    {
-	Color cOldColor = iColour;
-	g.setColor (Color.white);
-	g.fillRect (iCentreX - iWidth / 2, iCentreY - iHeight / 2, iWidth + 1, iHeight + 1);
-	setColor (cOldColor);
-    }
 }
--- PileClass.java
+++ PileClass.java
@@ -1,57 +1,38 @@
 import java.awt.*;
 import java.util.*;
 
-public class PileClass extends DeckClass
-{
-    boolean firstGeneration = true;
+public class PileClass extends DeckClass {
 
-    public int numberOfFaceUpCards ()
-    {
-	for (int i = 0 ; i < deck.size () ; i++)
-	{
-	    if (((CardClass) deck.get (i)).getSide () == 1)
-	    {
-		return getDeckLength () - i;
-	    }
+	public int numberOfFaceUpCards() {
+		for (int i = 0; i < deck.size(); i++) {
+			if (((CardClass) deck.get(i)).getSide() == 1) {
+				return getDeckLength() - i;
+			}
+		}
+		return 0;
 	}
-	return 0;
-    }
 
+	public boolean isPointInsideFaceUpCards(int x, int y) {
+		// check if point is inside face up cards
 
-    public boolean isPointInsideFaceUpCards (int x, int y)
-    {
-	// check if point is inside face up cards
-
-	for (int i = 0 ; i < deck.size () ; i++)
-	{
-	    if (((CardClass)(deck.get (i))).isPointInside (x, y))
-	    {
-		return true;
-	    }
+		for (int i = 0; i < deck.size(); i++) {
+			if (((CardClass) (deck.get(i))).isPointInside(x, y)) {
+				return true;
+			}
+		}
+		return false;
 	}
-	return false;
-    }
 
-
-    public void draw (Graphics g)
-    {
-	for (int i = 0 ; i < deck.size () ; i++)
-	{
-	    CardClass c1 = (CardClass) deck.get (i);
-	    c1.setSize (size);
-	    c1.setCentre (iCentreX, iCentreY + i * 25);
-	    if (i < deck.size () - 1 && firstGeneration)
-	    {
-		c1.setSide (0);
-	    }
-	    c1.draw (g);
+	public void draw(Graphics g) {
+		for (int i = 0; i < deck.size(); i++) {
+			CardClass c1 = (CardClass) deck.get(i);
+			c1.setSize(size);
+			c1.setCentre(iCentreX, iCentreY + i * 25);
+			c1.draw(g);
+		}
 	}
-	firstGeneration = false;
-    }
 
-
-    public void erase (Graphics g)
-    {
-	// TODOs
-    }
+	public void erase(Graphics g) {
+		// TODOs
+	}
 }
